c8fc6320a24748b195b776b9fb818d00c1895f05
lishouqian <lishouqian@stary.com>
2024-01-08 20:17:57 +0800

    hotfix shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 PANDORA_NEXT_DATA="/data"
 ## 环境变量的值
-sed -i "s/\"license_id\": \".*\"/\"license_id\": \"$license_id\"/" /data/config.json
-./PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
+sed -i "s/\"license_id\": \".*\"/\"license_id\": \"$license_id\"/" data/config.json
+chmod -x PandoraNext
+PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
