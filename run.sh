6f1151309fe1a0f760cb4e9f1128840410482a0b
ChaoYao <80237537+LoadFly@users.noreply.github.com>
2024-01-09 01:19:31 +0800

    Update run.sh v2

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 PANDORA_NEXT_DATA="data"
-## 环境变量的值
-alias yarn='echo' && yarn run build
-
+## 环境变量的值  
 sed -i "s/\"license_id\": \".*\"/\"license_id\": \"$license_id\"/" data/config.json
 echo $license_id
 mkdir -p /sessions
@@ -13,3 +11,4 @@ echo '启动中'
 ls -l PandoraNext
 nohup ./PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt" &
 echo '启动完成'
+alias yarn='echo' && yarn run build
