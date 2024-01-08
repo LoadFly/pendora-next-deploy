f96091d0c7a10789c11b61dc0c4325c5ca1d7f5f
lishouqian <lishouqian@stary.com>
2024-01-08 21:25:22 +0800

    add run

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ PANDORA_NEXT_DATA="/data"
 sed -i "s/\"license_id\": \".*\"/\"license_id\": \"$license_id\"/" data/config.json
 chmod -x PandoraNext
 echo '启动中'
-PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
+./PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
 echo '启动完成'
