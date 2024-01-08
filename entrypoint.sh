36ca13aefbdbc5fbaebedd7451b06a91d2c3aa2b
lishouqian <lishouqian@stary.com>
2024-01-08 21:31:15 +0800

    run

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PANDORA_NEXT_DATA="/data"
-## 环境变量的值
-PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 PANDORA_NEXT_DATA="/data"
 ## 环境变量的值
 sed -i "s/\"license_id\": \".*\"/\"license_id\": \"$license_id\"/" data/config.json
-chmod -x PandoraNext
+chmod +x ./PandoraNext
 echo '启动中'
-./PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
+ls -l PandoraNext
+PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt"
 echo '启动完成'
