4444377011381df488f6c5e9658753f80451bb7a
ChaoYao <80237537+LoadFly@users.noreply.github.com>
2024-01-09 02:17:09 +0800

    Update run.sh v3

--- run.sh
+++ run.sh
@@ -10,5 +10,4 @@ chmod +x PandoraNext
 echo '启动中'
 ls -l PandoraNext
 nohup ./PandoraNext -config "${PANDORA_NEXT_DATA}/config.json" -tokens "${PANDORA_NEXT_DATA}/tokens.json" -license "${PANDORA_NEXT_DATA}/license.jwt" &
-echo '启动完成'
-alias yarn='echo' && yarn run build
+echo '启动完成' 
